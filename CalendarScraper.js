68c75fd05ba979d9027c21eef83787b26a05dc31
Shamar Edwards <shamardedwards@Shamars-MacBook-Air.local>
2023-07-13 15:56:10 -0600

    initial commit

--- CalendarScraper.js
+++ CalendarScraper.js
@@ -1,91 +1,84 @@
-#!/usr/bin/node
-'use strict';
 const fs = require('fs');
-const {google} = require('googleapis');
-//Below needs to point to a .json file with the API key and Calendar ID
-const API_KEY_PATH = '/home/pi/google_api_key.json';
-/*The Google calendar needs to be publicly readabl; this avoids using OAuth2
-which is impossible to do with a non-publicly accessible device as Google
-has eliminated the out-of-band OAuth2 method of getting a refresh token */
-const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
-const UPDATE_INTERVAL = 30; //How often to poll the Google calendar in mins
-const INTERVAL_OVERLAP = 1; //Overlap in min- to be sure we don't miss anything
-const MAX_API_ERRORS = 10; // how many API requests to retry if errors
-const API_RETRY_DELAY = 1; //how long in mins to wait between API retries
-//Globals
-let pendingCommands = null; //Mutex for pending unexecuted commands
-let errorsInAPI = 0; //Counter to limit requests to Google
-// Load API Key & Cal ID from a local json file
-fs.readFile(API_KEY_PATH, (err, content) => {
-  if (err) return logWithTime('Error loading API key file:', err);
-  // Authorize a client with credentials, then call the Google Calendar API.
-  const obj = JSON.parse(content);
-  planEvents(obj.api_key, obj.google_cal_id); //Do a plan now
-  setInterval(planEvents, UPDATE_INTERVAL*60000, obj.api_key, obj.google_cal_id); //Replan
-  logWithTime('Program Successfully Started');
-});
-//Plan out the current interval
-function planEvents(api_key, google_cal_id) {
-  const calendar = google.calendar({version: 'v3', auth: api_key});
-  const planStartDate = new Date;
-  planStartDate.setSeconds(planStartDate.getSeconds() + 10); //Give us a ten sec delay for causality
-  const planEndDate = new Date(planStartDate.getTime()); //Clone current time
-  planEndDate.setMinutes(planStartDate.getMinutes() + UPDATE_INTERVAL + INTERVAL_OVERLAP); // Next interval
-  if (pendingCommands) { //We have planned commands pending, this happens especially if commands take time
-    logWithTime('We had pending events at: ' + pendingCommands.toString());
-    let delay = pendingCommands - Date.now() + 10;
-    if (delay < UPDATE_INTERVAL * 60000 ) { //if the normal update won't get the next event
-      setTimeout(planEvents,delay, api_key, google_cal_id); //Call ourself in the future
-      logWithTime('Recall planEvents() in ' + delay + ' ms');
+const { google } = require('googleapis');
+const XLSX = require('xlsx');
+
+const API_KEY_PATH = './google_api_key.json';
+
+// Load JSON file
+const apiKeyData = fs.readFileSync(API_KEY_PATH);
+const { api_key, calendar_id } = JSON.parse(apiKeyData);
+
+// Set up Google Calendar API client
+const calendar = google.calendar({ version: 'v3', auth: api_key });
+
+// Set the start date to March 1, 2017
+const startDate = new Date('2017-03-01T00:00:00Z');
+
+// Set the end date to March 31, 2023
+const endDate = new Date('2023-03-31T23:59:59Z');
+
+// Create a new workbook
+const workbook = XLSX.utils.book_new();
+
+// Retrieve events for each year from 2017 to 2023
+for (let year = 2017; year <= 2023; year++) {
+  // Set the start and end dates for the specific year
+  startDate.setFullYear(year);
+  endDate.setFullYear(year);
+
+  // Create a new worksheet for the year
+  const worksheet = XLSX.utils.aoa_to_sheet([['Email Address', 'Event Description']]);
+
+  // Set to store unique email addresses
+  const uniqueEmails = new Set();
+
+  // Retrieve events within the specified time range for the year
+  calendar.events.list(
+    {
+      calendarId: calendar_id,
+      timeMin: startDate.toISOString(),
+      timeMax: endDate.toISOString(),
+      singleEvents: true,
+      orderBy: 'startTime',
+    },
+    (err, res) => {
+      if (err) {
+        console.error('The Google API request returned an error:', err.message);
+        return;
+      }
+
+      const events = res.data.items;
+      if (events.length) {
+        console.log(`Email addresses and event descriptions found in Google Calendar events for ${year}:`);
+
+        events.forEach((event) => {
+          if (event.description) {
+            const emailRegex = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/g; // Regular expression for email address matching
+            const matches = event.description.match(emailRegex); // Extract email addresses from event description
+
+            if (matches) {
+              matches.forEach((email) => {
+                if (!email.endsWith('@kemptonasset.com')) {
+                  uniqueEmails.add(email);
+                  XLSX.utils.sheet_add_aoa(worksheet, [[email, event.description]], { origin: -1 });
+                }
+              });
+            }
+          }
+        });
+
+        // Add the worksheet to the workbook with the year as the tab name
+        XLSX.utils.book_append_sheet(workbook, worksheet, `${year}`);
+      } else {
+        console.log(`No events found in the specified time range for ${year}.`);
+      }
+
+      // Save the workbook as an Excel file after processing all years
+      if (year === 2023) {
+        const excelFilename = `prospects_emails_year.xlsx`;
+        XLSX.writeFile(workbook, `${__dirname}/temp/${excelFilename}`);
+        console.log(`Email addresses and event descriptions saved to ${excelFilename}`);
+      }
     }
-    return; // don't plan
-  }
-  calendar.events.list({
-    calendarId: google_cal_id,
-    timeMin: planStartDate.toISOString(),
-    timeMax: planEndDate.toISOString(),
-    maxResults: 10, //Change this for a busy calendar
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) {
-      logWithTime('The Google API request returned an error: ' + err);
-      errorsInAPI += 1;
-      logWithTime('We have ' + errorsInAPI + ' errors in a row');
-      if (errorsInAPI < MAX_API_ERRORS) { //Limit request retries
-        setTimeout(planEvents,API_RETRY_DELAY * 60000,api_key,google_cal_id); //Call ourself in the future
-        logWithTime('Will Recall planEvents()');
-      }
-      else {
-        logWithTime('We gave up on this planning interval due to excess retries');
-      }
-      return; //we didn't get an event, so give up
-    }
-    errorsInAPI = 0; //Got a good API request, so reset error counter
-    for (let event of res.data.items) { //unpack multiple events
-      const eventStart = new Date(event.start.dateTime);
-      const eventEnd = new Date(event.end.dateTime);
-      if ( eventStart >= planStartDate ) { //Do something for an event start
-        setTimeout(logWithTime,eventStart - Date.now(), 'Event Named: ' + event.summary + ' begins');
-        /* Below is the mutex functionality if a calendar even triggers a complex chain
-        of events that takes time and you don't want multiple events colliding
-        it's not necessary when the events trigger something quick   */
-        pendingCommands = new Date(eventStart - Date.now()); //keep track of when the last event is expected to end
-        setTimeout(clear_commands,eventStart - Date.now()); //clear the mutex when commands are done
-      }
-      if ( eventEnd <= planEndDate ) { //Do something for an end
-        setTimeout(logWithTime,eventEnd - Date.now(), 'Event Named: ' + event.summary + ' ends');
-        pendingCommands = new Date(eventEnd - Date.now()); //keep track of when the last event is expected to end
-        setTimeout(clear_commands,eventEnd - Date.now()); //clear the mutex when commands are done
-      }
-    }
-  });
-}
-function clear_commands() { //clears our mutex
-  pendingCommands = null;
-}
-//log the time with milliseconds to verify time
-function logWithTime(message) {
-  const curDate = new Date();
-  console.log(curDate.toLocaleString('en-GB') + ':' + curDate.getMilliseconds() + ': ' + message);
+  );
 }
